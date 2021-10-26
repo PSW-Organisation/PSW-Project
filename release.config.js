255e9a38bb7c42bf740981d08d13148efde2842a
RatkoSebez <sebezratko@gmail.com>
2021-10-26 20:52:38 +0200

    feat: setting up repository

---
+++ release.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    branches: "develop",
+    repositoryUrl: "https://github.com/RatkoSebez/PSW-Projekat",
+    plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/npm', '@semantic-release/github']
+}
