951ca5d43672fdbbefd26a440d1bcf2c6fbf7725
RatkoSebez <sebezratko@gmail.com>
2021-10-29 16:25:15 +0200

    feat: enforce conventional commits

--- release.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    branches: "develop",
-    repositoryUrl: "https://github.com/RatkoSebez/PSW-Projekat",
-    plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/npm', '@semantic-release/github']
-}
