56af7e5f9c95b339717063c8a037add0976842f2
kristinaatamindzija <tamindzija.ra180.2018@uns.ac.rs>
2021-11-01 20:41:55 +0100

    feat:  validation and made connection with database
    feat: created the feedback component in hospital frontend and the form for sending a patients feedback
    
    feat: added favicon, imported lottie animation library for angular and created welcome page component for patients portal
    
    feat: implemented sending feedbacks in angular project, defined validation rule for frontend and displayed notifications after sending feedback

--- Hospital/HospitalFrontend/src/app/app.module.ts
+++ Hospital/HospitalFrontend/src/app/app.module.ts
@@ -1,21 +1,49 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PecurkaComponent } from './pecurka/pecurka.component';
 import { ConfigService } from './config/config.service';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { RouterModule } from '@angular/router';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LottieModule } from 'ngx-lottie';
+import player from 'lottie-web';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ToastrModule } from 'ngx-toastr';
+
+export function playerFactory() {
+  return player;
+}
 
 @NgModule({
   declarations: [
     AppComponent,
-    PecurkaComponent
+    PecurkaComponent,
+    FeedbackComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-custom',
+      progressBar: true,
+      progressAnimation: 'increasing'
+    }),
+    RouterModule.forRoot([
+      { path: '', component: WelcomeComponent},
+      { path: 'feedback', component: FeedbackComponent}
+    ]), 
+    NgbModule,
+    LottieModule.forRoot({ player: playerFactory }),
   ],
   providers: [ConfigService],
   bootstrap: [AppComponent]
---
+++ Hospital/HospitalLibrary/Model/PatientFeedback.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ehealthcare.Model
+{
+    public class PatientFeedback
+    {
+        public string id { get; set; }
+        public string patientId { get; set; }
+        public string text { get; set; }
+        public bool anonymous { get; set; }
+        public bool publishAllowed { get; set; }
+
+    }
+}
