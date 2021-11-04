1a9602cb6e0398da107f0d6fe05b029c6df5be36
Darko Vrbaski <vrbaski.ra200.2018@uns.ac.rs>
2021-11-04 02:14:44 +0100

    feat: redesigned floor selector, installed angular material
    feat: display floors in the selected buiding

--- Hospital/ManagerFrontend/src/app/app-routing.module.ts
+++ Hospital/ManagerFrontend/src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PharmaciesViewComponent } from './pharmacies-view/pharmacies-view.component';
 
-const routes: Routes = [{path: 'pharmacies', component: PharmaciesViewComponent}];
+
+import { BuildingFloorsComponent } from './building-floors/building-floors.component';
+import { HospitalExteriorViewComponent } from './hospital-exterior-view/hospital-exterior-view.component';
+
+const routes: Routes = [{path: 'pharmacies', component: PharmaciesViewComponent},
+  { path: "building/:buidingId/floor/:id", component: BuildingFloorsComponent },
+  { path: "", component: HospitalExteriorViewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
