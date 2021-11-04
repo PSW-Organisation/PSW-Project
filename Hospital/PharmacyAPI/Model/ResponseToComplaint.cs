7b686a75a0433175f770d1914e30a2b3e3d42dae
Boris Horvat <horvat.boris99@gmail.com>
2021-11-04 19:47:50 +0100

    feat: added hospital and reponse dto, adapters and models and controllers seeded database

---
+++ Hospital/PharmacyAPI/Model/ResponseToComplaint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmacyAPI.Model
+{
+   public  class ResponseToComplaint
+    {
+        private long responseToComplaintId;
+        private DateTime date;
+        private String content;
+
+        public long ResponseToComplaintId { get => responseToComplaintId; set => responseToComplaintId = value; }
+
+        public DateTime Date { get => date; set => date = value; }
+        public String Content { get => content; set => content = value; }
+
+        public ResponseToComplaint() { }
+
+        public ResponseToComplaint(long responseId, DateTime date, string content)
+        {
+            this.responseToComplaintId = responseId;
+            this.date = date;
+            this.content = content;
+        }
+    }
+}
--- Hospital/PharmacyAPI/PharmacyDbContext.cs
+++ Hospital/PharmacyAPI/PharmacyDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PharmacyAPI.Model;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,7 +9,8 @@ namespace PharmacyAPI
     public class PharmacyDbContext : DbContext
     {
         public DbSet<Pharmacy> Pharmacies { get; set; }
-
+        public DbSet<Hospital> Hospitals { get; set; }
+        public DbSet<ResponseToComplaint> ResponsesToComplaint { get; set; }
         public PharmacyDbContext(DbContextOptions<PharmacyDbContext> options) : base(options) { }
 
         protected PharmacyDbContext() { }
@@ -22,6 +24,22 @@ namespace PharmacyAPI
                 PharmacyAddress = "Bul. Cara Lazara 58",
                 PharmacyUrl = "",
                 PharmacyApiKey = ""
+            });
+            modelBuilder.Entity<ResponseToComplaint>().HasData(new ResponseToComplaint()
+            {
+                ResponseToComplaintId = 1,
+                Date = DateTime.Now,
+                Content = "Imali smo problema sa nabavkom leka panadol, izvinjavamo se na zakasneloj porudzbini"
+               
+            }); ;
+            
+            modelBuilder.Entity<Hospital>().HasData(new Hospital()
+            {
+                HospitalId = 1,
+                HospitalName = "Institut za zdravstvenu zastitu dece i omladine Vojvodine",
+                HospitalAddress = "Kralja Petra 32",
+                HospitalApiKey = "",
+                HospitalUrl = "",
             });
         }
     }
