75c1fb96cc04ec7c7a22a4f65acf88b0a462f87e
Darko Vrbaski <vrbaski.ra200.2018@uns.ac.rs>
2021-11-05 22:07:08 +0100

    fix: fixed bootsrap different versions conflicts

--- Hospital/ManagerFrontend/src/app/welcome/welcome.component.ts
+++ Hospital/ManagerFrontend/src/app/welcome/welcome.component.ts
@@ -9,7 +9,18 @@ export class WelcomeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  public loadExternalScript(url: string) {
+    const body = <HTMLDivElement>document.body; const script =
+      document.createElement('script'); script.innerHTML = ''; script.src = url; script.async = true; script.defer = true;
+    body.appendChild(script);
+  } 
+  
+  ngOnInit() {
+    this.loadExternalScript("https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js");
+    this.loadExternalScript("https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js");
+    this.loadExternalScript("../../assets/scripts/jquery-3.2.1.min.js");
+    this.loadExternalScript("../../assets/scripts/all-plugins.js");
+    this.loadExternalScript("../../assets/scripts/plugins-activate.js");
   }
 
 }
