f7acac2e5c83f6eb123859c113b4330dd2c84572
Darko Vrbaski <vrbaski.ra200.2018@uns.ac.rs>
2021-11-05 22:07:08 +0100

    fix: fixed bootsrap different versions conflicts

--- Hospital/ManagerFrontend/src/app/app-routing.module.ts
+++ Hospital/ManagerFrontend/src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PharmaciesViewComponent } from './pharmacies-view/pharmacies-view.component';
-
-
 import { BuildingFloorsComponent } from './building-floors/building-floors.component';
 import { HospitalExteriorViewComponent } from './hospital-exterior-view/hospital-exterior-view.component';
 import { WelcomeComponent } from './welcome/welcome.component';
