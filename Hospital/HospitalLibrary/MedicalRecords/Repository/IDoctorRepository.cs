8fc24c68a44dfa9dcec85b31339a5a076293f3fe
kristinaatamindzija <tamindzija.ra180.2018@uns.ac.rs>
2021-11-20 22:40:15 +0100

    fix: migrations deleted and doctor repo implements generic string id repo

--- Hospital/HospitalLibrary/MedicalRecords/Repository/IDoctorRepository.cs
+++ Hospital/HospitalLibrary/MedicalRecords/Repository/IDoctorRepository.cs
@@ -1,12 +1,13 @@
 ﻿using ehealthcare.Model;
 using HospitalLibrary.Repository;
+using HospitalLibrary.Repository.DbRepository;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HospitalLibrary.MedicalRecord.Repository
 {
-    public interface IDoctorRepository : IGenericRepository<Doctor>
+    public interface IDoctorRepository : IGenericSTRINGIDRepository<Doctor>
     {
         public int FindLeastNumberOfPatient();
         List<Doctor> GetLeastOccupiedDoctors(int minPatients);
