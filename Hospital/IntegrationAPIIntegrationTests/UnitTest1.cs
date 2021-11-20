3d982b18bf12cce1c7b0f9f1c8d0b131848edfae
nevenaatic <atic.ra67.2018@uns.ac.rs>
2021-11-20 13:27:13 +0100

    feat: created projects for tests

---
+++ Hospital/IntegrationAPIIntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace IntegrationAPIIntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Hospital/IntegrationAPIUnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace IntegrationAPIUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
