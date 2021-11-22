2eef08727e0961b661243f4f4d5ddf0c5ac25c9f
Milica <samardzija.ra66.2018@uns.ac.rs>
2021-11-22 22:44:09 +0100

    feat: integration tests for pharmacy

--- Hospital/IntegrationAPIUnitTests/OrderingMedicineTest.cs
+++ Hospital/IntegrationAPIUnitTests/OrderingMedicineTest.cs
@@ -12,39 +12,6 @@ namespace IntegrationAPIUnitTests
     public class OrderingMedicineTest
     {
 
-        //integracioni za hospital
-        [Fact]
-        public void search_medicine_found()
-        {
-            var stubRepositoryPharmacy = new Mock<PharmacyRepository>();
-            var stubRepositoryMedicine = new Mock<MedicineRepository>();
-            var pharmacies = new List<Pharmacy>();
-
-            pharmacies.Add(new Pharmacy("http://localhost:29631", "Flos", "Bulevar Oslobodjenja, Novi Sad", "bc56df25-0d34-4801-b76a-931e61b4c752", "108817cf-dc25-40f4-a18f-244c1315840a"));
-            stubRepositoryPharmacy.Setup(p => p.GetAll()).Returns(pharmacies);
-            IPharmacyService pharmacyService = new PharmacyService(stubRepositoryPharmacy.Object);
-            MedicineService medicineService = new MedicineService(stubRepositoryMedicine.Object, pharmacyService);
-
-            List<Pharmacy> retVal = medicineService.searchMedicine("panklav", 1);
-
-            Assert.True(retVal.Count.ToString().Equals("1"));
-        }
-
-        [Fact]
-        public void search_medicine_not_found()
-        {
-            var stubRepositoryPharmacy = new Mock<PharmacyRepository>();
-            var stubRepositoryMedicine = new Mock<MedicineRepository>();
-            var pharmacies = new List<Pharmacy>();
-
-            pharmacies.Add(new Pharmacy("http://localhost:29631", "Flos", "Bulevar Oslobodjenja, Novi Sad", "bc56df25-0d34-4801-b76a-931e61b4c752", "108817cf-dc25-40f4-a18f-244c1315840a"));
-            stubRepositoryPharmacy.Setup(p => p.GetAll()).Returns(pharmacies);
-            IPharmacyService pharmacyService = new PharmacyService(stubRepositoryPharmacy.Object);
-            MedicineService medicineService = new MedicineService(stubRepositoryMedicine.Object, pharmacyService);
-
-            List<Pharmacy> retVal = medicineService.searchMedicine("panklav", 1);
-
-            Assert.True(retVal.Count.ToString().Equals("0"));
-        }
+        
     }
 }
