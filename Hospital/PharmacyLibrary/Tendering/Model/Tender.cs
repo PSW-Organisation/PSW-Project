af7876477bc4cb73cea6516f3d6141a05ad114f2
nikolaivanovic <ivanovic.ra1.2018@uns.ac.rs>
2022-01-09 13:37:56 +0100

    fix: changed model

--- Hospital/PharmacyLibrary/Tendering/Model/Tender.cs
+++ Hospital/PharmacyLibrary/Tendering/Model/Tender.cs
@@ -13,6 +13,7 @@ namespace PharmacyLibrary.Tendering.Model
         private bool _open;
         //private List<TenderResponse> _tenderResponses;
         private string _apiKeyPharmacy;
+        private bool _isWon;
 
         public virtual List<TenderItem> TenderItems { get => _tenderItems; set => _tenderItems = value; }
         public DateTime TenderOpenDate { get => _tenderOpenDate; set => _tenderOpenDate = value; }
@@ -22,6 +23,7 @@ namespace PharmacyLibrary.Tendering.Model
         public bool Open { get => _open; set => _open = value; }
         //public virtual List<TenderResponse> TenderResponses { get => _tenderResponses; set => _tenderResponses = value; }
         public string ApiKeyPharmacy { get => _apiKeyPharmacy; set => _apiKeyPharmacy = value; }
+        public bool IsWon { get => _isWon; set => _isWon = value; }
 
         public Tender() : base(-1) { }
     }
