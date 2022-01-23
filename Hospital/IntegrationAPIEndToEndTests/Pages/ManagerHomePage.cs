5ae71c225e22c9981f781ea2c950aacf35e59a67
Milica <samardzija.ra66.2018@uns.ac.rs>
2022-01-23 19:15:21 +0100

    feat: initialization

---
+++ Hospital/IntegrationAPIEndToEndTests/Pages/ManagerHomePage.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace IntegrationAPISeleniumTests.Pages
+{
+   public class ManagerHomePage
+   {
+        private readonly IWebDriver driver;
+        public string ManagerHomepageURI { get { return "http://localhost:4200/home"; } }
+        private IWebElement PharmaciesButton => driver.FindElement(By.Id("pharmacies"));
+        private IWebElement PharmaciesCard => driver.FindElement(By.Name("pharmacies"));
+
+        public void PharmaciesCardDisplayed()
+        {
+            //return PharmaciesCard.Displayed;
+            PharmaciesButton.Click();
+        }
+
+        public ManagerHomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void WaitUntilPharmaciesRedirect()
+        {
+            var wait = new WebDriverWait(driver, new TimeSpan(0, 0, 10));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.UrlContains("pharmacies"));
+        }
+
+        public void Navigate(string url) => driver.Navigate().GoToUrl(url);
+
+        }
+}
